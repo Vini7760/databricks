c4e2bcae5e6ffe6bb43331f0ce8b8367d23c2427
Vini7760 <Vini7760>
2024-07-19 08:13:37 +0000

    added
    done

--- v2.2.10/ASP 1 - Introductions/ASP 1.1 - Databricks Platform.py
+++ v2.2.10/ASP 1 - Introductions/ASP 1.1 - Databricks Platform.py
@@ -1,7 +1,7 @@
 # Databricks notebook source
 # MAGIC %md-sandbox
 # MAGIC
-
+# MAGIC
 # MAGIC <div style="text-align: center; line-height: 0; padding-top: 9px;">
 # MAGIC   <img src="https://databricks.com/wp-content/uploads/2018/03/db-academy-rgb-1200px.png" alt="Databricks Learning" style="width: 600px">
 # MAGIC </div>
@@ -60,6 +60,7 @@
 
 # COMMAND ----------
 
+print("Run default language")
 print("Run default language")
 
 # COMMAND ----------
